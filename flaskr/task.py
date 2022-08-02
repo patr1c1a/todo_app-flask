7190a51feb139b56c4b8bcef9f7589d1430246a9
Patricia Miguel <contacto@patriciaemiguel.com>
2022-08-02 20:27:54 -0300

    Added type hints and made Task status field an enum with only 2 possible values

--- flaskr/task.py
+++ flaskr/task.py
@@ -1,10 +1,22 @@
+import enum
+
+
+class TaskStatus(enum.Enum):
+	PENDING = 1
+	DONE = 2
+
+
 class Task:
-	def __init__(self, id, name, status):
-		self.id = id
+	def __init__(self, task_id: int, name: str, status: TaskStatus) -> None:
+		self.id = task_id
 		self.name = name
 		self.status = status
 
-	def serialize(self):
+	def serialize(self) -> dict:
+		"""
+		Converts class attributes to a dict so it can be easily serialized into JSON.
+		:return: dict
+		"""
 		return {"task_id": self.id,
 			"task_name": self.name,
 			"task_status": self.status}
