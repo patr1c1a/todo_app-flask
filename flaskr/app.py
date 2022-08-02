76c94abbdf903494d352d406d4bed09e744b9bd6
Patricia Miguel <contacto@patriciaemiguel.com>
2022-08-02 20:27:54 -0300

    Added type hints and made Task status field an enum with only 2 possible values

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,11 +1,16 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, Response
 from flaskr.task_manager import TaskManager
 
 app = Flask(__name__)
 
 
 @app.route('/add/')
-def add():
+def add() -> Response:
+    """
+    Endpoint (supported verbs: GET).
+    Gets parameters from the request to create a new Task object, then adds it to storage.
+    :return: Response (JSON)
+    """
     task_name = request.args.get("task_name")
     task_status = request.args.get("task_status")
     result = manager.add_new_task(task_name, task_status)
