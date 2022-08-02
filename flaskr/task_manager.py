c0315ff9e0d1e8b4a61f6f5455c999ca6ebeeb1d
Patricia Miguel <contacto@patriciaemiguel.com>
2022-08-02 20:27:54 -0300

    Added type hints and made Task status field an enum with only 2 possible values

--- flaskr/task_manager.py
+++ flaskr/task_manager.py
@@ -2,12 +2,18 @@ from flaskr.task import Task
 
 
 class TaskManager:
-	def __init__(self):
+	def __init__(self) -> None:
 		self.storage = []
 
-	# storing in a list for now. TODO: change storage to database
-	def add_new_task(self, task_name, task_status):
-		next_id = len(self.storage)+1  # will be replaced with database generated id
+	def add_new_task(self, task_name, task_status) -> list:
+		"""
+		Creates a new Task with the given parameters and then stores it. The storage uses a list for now.
+		TODO: change storage to database. next_id will be replaced with a database generated id.
+		:param task_name: str
+		:param task_status:
+		:return:
+		"""
+		next_id = len(self.storage)+1
 		task = Task(next_id, task_name, task_status)
 		self.storage.append(task)
 		return self.storage
