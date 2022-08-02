b851f6958a7fdcaca58b8de506767baada9af946
Patricia Miguel <contacto@patriciaemiguel.com>
2022-08-02 10:08:13 -0300

    Add id to Task class, to mimick database behavior

--- flaskr/task_manager.py
+++ flaskr/task_manager.py
@@ -7,6 +7,7 @@ class TaskManager:
 
 	# storing in a list for now. TODO: change storage to database
 	def add_new_task(self, task_name, task_status):
-		task = Task(task_name, task_status)
+		next_id = len(self.storage)+1  # will be replaced with database generated id
+		task = Task(next_id, task_name, task_status)
 		self.storage.append(task)
 		return self.storage
